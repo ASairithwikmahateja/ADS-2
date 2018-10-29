73c82d7820d19ba245cedf760884aa85b4a7185b
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-10-29 20:20:19 +0530

    "ads2_m1_a1_1: 4/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M 1/1.1 Graph/Bag.java
+++ M 1/1.1 Graph/Bag.java
@@ -1,32 +1,30 @@
-/*************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *
- *  A generic bag or multiset, implemented using a linked list.
- *
- *************************************************************************/
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of
- *  generic items. It supports insertion and iterating over the
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
-
-    // helper linked list class
+    /**
+     * number of elements in bag.
+     */
+    private int N;
+    /**
+     * beginning of bag.
+     */
+    private Node first;
+    /**
+     * helper linked list class
+     */
     private class Node {
+        /**
+         * The item.
+         */
         private Item item;
+        /**
+         * The node.
+         */
         private Node next;
     }
 
@@ -63,7 +61,6 @@ public class Bag<Item> implements Iterable<Item> {
         N++;
     }
 
-
    /**
      * Return an iterator that iterates over the items in the bag.
      */
@@ -73,11 +70,25 @@ public class Bag<Item> implements Iterable<Item> {
 
     // an iterator, doesn't implement remove() since it's optional
     private class ListIterator implements Iterator<Item> {
+        /**
+         * The Node.
+         */
         private Node current = first;
-
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
         public boolean hasNext()  { return current != null;                     }
+        /**
+         * void.
+         */
         public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * The Item.
+         *
+         * @return     Item.
+         */
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
