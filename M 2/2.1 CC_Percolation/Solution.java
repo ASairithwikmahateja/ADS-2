17b63711d19de7192cd3d35abb1941d77576e7d0
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-10-30 19:46:32 +0530

    "ads2_m2_a1_1: 1/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 2/2.1 CC_Percolation/Solution.java
+++ M 2/2.1 CC_Percolation/Solution.java
@@ -1,59 +1,8 @@
 import java.util.Scanner;
 /**
- * Class for percolation.
- */
-class Percolation {
-    /**
-     * matrix array of type boolean which stores the open and closed sites.
-     */
-    private boolean[][] matrix;
-    /**
-     * Virtual node top for top rows.
-     */
-    private int top = 0;
-    /**
-     * Virtual node bottom for bottom rows.
-     */
-    private int bottom;
-    /**
-     * size variable to keep track of the size of matrix.
-     */
-    private int size;
-    /**
-     * newgraph object.
-     */
-    private Newgraph g;
-    /**
-     * Constructs the object.
-     * @param      n     number of nodes.
-     */
-    Percolation(final int n) {
-        size = n;
-        bottom = size * size + 1;
-        g = new Newgraph(size * size + 2);
-        matrix = new boolean[size][size];
-    }
-    /**
-     * Open percolation.
-     *
-     * @param      v     { parameter_description }
-     * @param      w     { parameter_description }
-     */
-    public Graph open(int v, int w) {
-    	g.addEdge(v, w);
-    	return g;
-    }
-    /**
-     * Percolates method to find whether the matrix percolates or not.
-     * @return     True if percolates, False otherwise.
-     */
-    public boolean percolates(CC c) {
-    	System.out.println(c.connected(top, bottom));
-        return c.connected(top, bottom);
-    }
-}
-/**
  * class Solution.
+ * Time complexity for this method is O(N) since
+ * we are using while loop.
  */
 final class Solution {
 	/**
@@ -73,12 +22,8 @@ final class Solution {
 		Percolation p = new Percolation(n);
 		String[] str = new String[2];
 		while (sc.hasNext()) {
-			str = sc.nextLine().split(" ");
-			Graph temp = p.open(Integer.parseInt(str[0]),
-				Integer.parseInt(str[1]));
-			CC c = new CC(temp);
-			System.out.println(p.percolates(c));
-			break;
-		}
+			p.open(Integer.parseInt(str[0]) - 1,
+				Integer.parseInt(str[1]) + 1);
+		} System.out.println(p.percolates());
 	}
 }
