1ec451843d8e99ce1f9b35acfe058a3e458ee1d2
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-10-30 19:49:50 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M 2/2.1 CC_Percolation/Solution.java
+++ M 2/2.1 CC_Percolation/Solution.java
@@ -1,29 +1,26 @@
 import java.util.Scanner;
 /**
- * class Solution.
- * Time complexity for this method is O(N) since
- * we are using while loop.
+ * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-
-	}
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		Percolation p = new Percolation(n);
-		String[] str = new String[2];
-		while (sc.hasNext()) {
-			p.open(Integer.parseInt(str[0]) - 1,
-				Integer.parseInt(str[1]) + 1);
-		} System.out.println(p.percolates());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * main method that drives the program.
+     * @param      args  The arguments
+     * Time complexity for this method is O(N) since
+     * we are using while loop.
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertices = sc.nextInt();
+        Percolation p = new Percolation(vertices);
+        while (sc.hasNext()) {
+            p.open(sc.nextInt() - 1, sc.nextInt() - 1);
+        }
+        System.out.println(p.percolates());
+    }
 }
