5248d4d40caa9dcf23fc8a883b422c17b10d0bd2
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-02 21:04:52 +0530

    "ads2_m5_ba1_1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ M 5/Bonus-1/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+	Solution() {
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String inp = sc.nextLine();
+		String[] inputs = inp.split(" ");
+		int v = Integer.parseInt(inputs[0]);
+		int e = Integer.parseInt(inputs[1]);
+		if (e == 0) {
+			System.out.println(e);
+		} else {
+			Graph gph = new Graph(v + 1);
+			while (sc.hasNext()) {
+			String connect = sc.nextLine();
+			String[] connects = connect.split(" ");
+			gph.addEdge(Integer.parseInt(connects[0]), Integer.parseInt(connects[1]));
+        }
+        CC connected = new CC(gph);
+        int parallelcount = 0;
+        int maxcount = 0;
+        int id = 0;
+        int[] idar = connected.idarray();
+        for (int i = 0; i < gph.vertices(); i++) {
+        	if (gph.hasParallelEdges(i)) {
+        		parallelcount++;
+        	}
+        	int idcount = 0;
+        	id = idar[i];
+        	for (int j = 0; j < idar.length; j++) {
+        		if (id == idar[j]) {
+        			idcount++;
+        		}
+        	}
+        	if (maxcount < idcount) {
+        	    maxcount = idcount;	
+        	}
+        }
+        System.out.println(parallelcount + maxcount);
+		}
+	}
+}
