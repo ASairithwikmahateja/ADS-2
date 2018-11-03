d6095d257ac6c77e12c6acca281e2227f9425051
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-03 11:48:54 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 6/ADS-2Week-1/Solution.java
+++ M 6/ADS-2Week-1/Solution.java
@@ -6,11 +6,30 @@ class PageRank {
 	/**
 	 * Constructs the object.
 	 */
-	Digraph g;
-	PageRank(Digraph dg) {
-		g = dg;
+	PageRank(Digraph g) {
+		double inirank = 1 / g.V();
+		int v = 0;
+		double newrank;
+	    if (g.adj(v) != null) {
+	    	newrank = inirank / g.outdegree(v);
+	    } else {
+	    	for (int w : g.adj(v)) {
+	    	    g.addEdge(v, w);
+	    	}
+	    }
 	}
-	
+
+	// public double getPR(int v) {
+	// 	for (int i = 0; i < 1000; i++) {
+	// 		return ;
+	// 	}
+	// }
+
+	// public String toString() {
+	// 	String str = " ";
+	// 	str = g.v + "- " + getPR(v);
+	// 	return str;
+	// }
 }
 
 class WebSearch {
@@ -23,19 +42,23 @@ public class Solution {
 		// read the first line of the input to get the number of vertices
         Scanner sc = new Scanner(System.in);
         int nvert = Integer.parseInt(sc.nextLine());
-
+        Digraph dg = new Digraph(nvert);
 		// iterate count of vertices times 
 		// to read the adjacency list from std input
 		// and build the graph
 		for (int i = 0; i < nvert; i++) {
-			String[] ed = sc.nextLine().split(" ");
+			String[] tok = sc.nextLine().split(" ");
+			for (int j = 0; j < tok.length; j++) {
+				dg.addEdge(Integer.parseInt(tok[0]),
+					Integer.parseInt(tok[j]));
+        	}
         }
-        Digraph dg = new Digraph(nvert);
+        System.out.println(dg.toString());
 		
 		// Create page rank object and pass the graph object to the constructor
 		PageRank pg = new PageRank(dg);
 		// print the page rank object
-		System.out.println(pg);
+		System.out.println(pg.toString());
 		
 		// This part is only for the final test case
 		
