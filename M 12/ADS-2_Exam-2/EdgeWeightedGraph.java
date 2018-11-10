46da9f0d1cd970f0aaa69b2f4a3271fe307a021a
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-10 10:41:53 +0530

    "ads2_m12_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 12/ADS-2_Exam-2/EdgeWeightedGraph.java
+++ M 12/ADS-2_Exam-2/EdgeWeightedGraph.java
@@ -180,7 +180,7 @@ public class EdgeWeightedGraph {
         for (int v = 0; v < ver; v++) {
             s.append(v + ": ");
             for (Edge e : adj[v]) {
-                s.append(e + " " + e.weight + "  ");
+                s.append(e.either() + " " + e.weight() + "  ");
             }
             s.append(NEWLINE);
         }
