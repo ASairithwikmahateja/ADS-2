afe34c6a37b7e97bec2728a1a6749ea3cf86fc62
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-10 12:25:13 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M 12/ADS-2_Exam-2/Solution.java
+++ M 12/ADS-2_Exam-2/Solution.java
@@ -65,7 +65,7 @@ public final class Solution {
 					d2.distTo(Integer.parseInt(pathe[2])));
 			String str = "";
 			for (Edge w : d1.pathTo(Integer.parseInt(pathe[1]))) {
-				if (w.either() == Integer.parseInt(pathe[0])) {
+				if (w.either() != Integer.parseInt(pathe[0])) {
 					str +=  pathe[0] + " ";
 				} else {
 					str += w.either() + " ";
