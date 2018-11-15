2f264bae587bcd0d1f76db44f05607dd14c3f56c
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-15 20:48:55 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 16/boggle/BoggleSolver.java
+++ M 16/boggle/BoggleSolver.java
@@ -1,65 +1,125 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
+/**
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-	TST<Integer> tst;
-	public BoggleSolver(String[] dictionary) {
-		tst = new TST<Integer>();
-		for (int i = 0; i < dictionary.length; i++) {
-			tst.put(dictionary[i], i);
-		}
-	}
-
-	// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public Iterable<String> getAllValidWords(BoggleBoard board) {
-		boolean[][] marked = new boolean[board.rows()][board.cols()];
-		ArrayList<String> list = new ArrayList<String>();
-		for (int i = 0; i < board.rows(); i++) {
-			for (int j = 0; j < board.cols(); j++) {
-				String c = "" + board.getLetter(i, j);
-				marked[i][j] = true;
-				dfs(list, c, i, j, marked, board);
-
+    /**
+     * dictionary of type TrieSET.
+     */
+    private TrieSET dict;
+    /**
+     * Constructs the object.
+     * Initializes the data structure using the given
+     array of strings as the dictionary.
+     (You can assume each word in the dictionary contains
+     only the uppercase letters A through Z.)
+     * @param      dictionary  The dictionary
+     */
+    public BoggleSolver(final String[] dictionary) {
+        dict = new TrieSET();
+        for (String s : dictionary) {
+            dict.add(s);
+        }
+    }
+    /**
+     * Gets all valid words.
+     * Returns the set of all valid words in the given
+     * Boggle board, as an Iterable.
+     * @param      board  The board
+     * @return     All valid words.
+     */
+    public Iterable<String> getAllValidWords(final BoggleBoard board) {
+        SET<String> validwords = new SET<String>();
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                boolean[][] marked = new boolean[board.rows()][board.cols()];
+                collect(board, i, j, marked, "", validwords);
             }
-		}
-		return list;
-	}
-	public void dfs(ArrayList<String> lst, String ch, int row, int col, boolean[][] marked, BoggleBoard board) {
-		if (ch.length() >= 3 && tst.contains(ch)) {
-			if (!lst.contains(ch)) {
-			    lst.add(ch);
-			}
-		}
-		if (!tst.hasPrefix(ch)) {
+        }
+        return validwords;
+    }
+    /**
+     * helper method for getAllValidWords method.
+     * @param      board   The board
+     * @param      row     The row
+     * @param      col     The col
+     * @param      marked  The marked
+     * @param      prefix  The prefix
+     * @param      set     The set
+     */
+    private void collect(final BoggleBoard board, final int row, final int col,
+                         final boolean[][] marked, final String prefix,
+                         final SET<String> set) {
+        if (marked[row][col]) {
             return;
-		}
-		for (int i = 0; i < board.rows(); i++) {
-			for (int j = 0; j < board.cols(); j++) {
-				if (marked[i][j] == true) {
-					continue;
-				}
-				ch += "" + board.getLetter(i, j);
-				marked[i][j] = true;
-				dfs(lst, ch, i, j, marked, board);
-            }
-		}
-	}
+        }
+        char letter = board.getLetter(row, col);
+        String word = prefix;
 
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
-	public int scoreOf(String word) {
-		if (word.length() == 4 || word.length() == 3) {
-			return 1;
-		} else if (word.length() == 5) {
-			return 2;
-		} else if (word.length() == 6) {
-			return 3;
-		} else if (word.length() == 7) {
-			return 5;
-		} else if (word.length() >= 8) {
-			return 11;
-		} return 0;
-	}
+        if (letter == 'Q') {
+            word += "QU";
+        } else {
+            word += letter;
+        }
+
+        if (!dict.hasPrefix(word)) {
+            return;
+        }
+
+        if (word.length() > 2 && dict.contains(word)) {
+            set.add(word);
+        }
+
+        marked[row][col] = true;
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                if ((row + i >= 0) && (row + i < board.rows())
+                    && (col + j >= 0) && (col + j < board.cols())) {
+                    collect(board, row + i, col + j, marked, word, set);
+                }
+            }
+        }
+
+        marked[row][col] = false;
+    }
+    /**
+     * Returns the score of the given word if it is in the dictionary,
+     zero otherwise.
+     (You can assume the word contains only the uppercase letters A through Z.)
+     * @param      word  The word
+     * @return     score of type integer.
+     */
+    public int scoreOf(final String word) {
+        final int three = 3;
+        final int four = 4;
+        final int five = 5;
+        final int six = 6;
+        final int seven = 7;
+        final int eleven = 11;
+        if (dict.contains(word)) {
+            switch (word.length()) {
+            case 0:
+            case 1:
+            case 2:
+                return 0;
+            case three:
+            case four:
+                return 1;
+            case five:
+                return 2;
+            case six:
+                return three;
+            case seven:
+                return five;
+            default:
+                return eleven;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
