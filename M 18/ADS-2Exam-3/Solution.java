dda99f7d5c143838c658d6e6316d05d531401866
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-11-17 11:38:05 +0530

    "ads2_m18_a1_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 18/ADS-2Exam-3/Solution.java
+++ M 18/ADS-2Exam-3/Solution.java
@@ -94,12 +94,12 @@ public class Solution {
 
 		for (int i = 0; i < str.length; i++) {
 			String[] temp = str[i].toLowerCase().split(" ");
-			for (int j = 0; j < temp.length; j++) {
-				System.out.println(temp[j]);
-				if (st.contains(temp[j])) {
-					st.put(temp[j], st.get(temp[j]) + 1);
-				} else {
+			st.put(temp[0], 0);
+			for (int j = 1; j < temp.length; j++) {
+				if (!st.contains(temp[j])) {
 					st.put(temp[j], st.get(temp[j]));
+				} else {
+					st.put(temp[j], st.get(temp[j]) + 1);
 				}
 			}
 		}
